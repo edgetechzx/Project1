68dab63b7cc20b866e695510e904fedb369c43c4
zx <edgetechzx@gmail.com>
2020-08-18 15:45:28 +0800

    second commit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/post', function () {
+    return "hello";
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
